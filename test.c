6779afbbc85401d03c2abb7a03607d98df8d0dba
namju-kim <cmsong1111@gmail.com>
2024-03-13 13:29:23 +0900

    C언어 반복문으로 인사 100번

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    // 너무너무 반가워서 100번 반복하고 싶어요
+    for(int i = 0;i<100;i++){
+        printf("Hello world!");
+    }
     return 0;
 }
